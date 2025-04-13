86bc0acef5d2a87cfd330b5ee8d61d105b5de4e3
sagar <s.g.devil.88@gmail.com>
2025-04-13 16:31:16 +0545

    update otp functions and more functionalities

--- authorization/auth.service.js
+++ authorization/auth.service.js
@@ -1,25 +1,38 @@
+import { mailOtp } from "../mail/mailer.js";
 import User from "../user/user.model.js";
 import { generateOTP } from "../utils/otp.js";
+import { Auth } from "./auth.model.js";
 
-export const optGenerator = async (req ,res ,next)=>{
+export const optGenerator = async (req, res, next) => {
+  const forgetEmail = req.body;
 
-   const forgetEmail = req.body;
+  if (!forgetEmail) {
+    return res.status(400).json({ message: "Email is required!!" });
+  }
 
-   if(!forgetEmail){
-    return res.status(400).json({message: "Email is required!!"})
-   }
+  //check email in database
+  const user = await User.findOne(forgetEmail);
 
-//check email in database 
-const user = await User.findOne(forgetEmail)
+  if (!user) {
+    return res.status(400).json({ message: "invalid credentials!!" });
+  }
 
-if(!user){
-    return res.status(400).json({message: "invalid credentials!!"})
+  const token = generateOTP();
+const email = forgetEmail.email
+ await mailOtp(forgetEmail.email, token);
+
+ 
+ //check if otp already exist with this email 
+
+ const existingAuth = await Auth.findOne(forgetEmail);
+
+ if(existingAuth)
+{
+    await Auth.updateOne({email},{$set:{token}})
 }
-
-const otp = generateOTP();
-
-console.log(otp)
-
-
-
+else{
+    await Auth.create({email,token})
 }
+return res.status(200).json({message: "OTP sent to your mail. "});
+
+};
---
+++ mail/mailer.js
@@ -0,0 +1,29 @@
+import nodemailer from "nodemailer"
+
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for port 465, false for other ports
+    auth: {
+      user: "wwwghimiresagar88@gmail.com",
+      pass: process.env.emailPassword,
+    },
+  });
+  
+  // async..await is not allowed in global scope, must use a wrapper
+  export const mailOtp = async(email,token) => {
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: '"Maddison Foo Koch 👻" <wwwghimiresagar88@gmail.com>', // sender address
+      to: "wwwghimiresagar88@gmail.com", // list of receivers
+      subject: "otp verification", // Subject line
+    //   text: "Hello world?", // plain text body
+      html:  `<div>Your otp code is <b>${token}</b></div>`, // html body
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+  }
+  
+  
